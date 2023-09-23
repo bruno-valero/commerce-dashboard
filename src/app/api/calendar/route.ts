52092010c82651288644768777d011de45134142
Bruno Fernandes <brunofvn6@gmail.com>
2023-09-23 17:39:33 -0300

    teste de apresentação inicial - correcao de erros

--- src/app/api/calendar/route.ts
+++ src/app/api/calendar/route.ts
@@ -1,4 +1,6 @@
+import { scheduleData } from '@/data/dummyTSX';
 import { redirect } from 'next/navigation';
+import { NextResponse } from 'next/server';
 
 export async function GET(req:Request):Promise<void> {
   const baseURL:string = process.env.BASE_URL as string;
@@ -7,9 +9,9 @@ export async function GET(req:Request):Promise<void> {
 };
 
 export async function POST(req:Request) {  
-  throw new Error('Este e um erro do calendario que o Bruno criou!')
-  // const data = await req.json();
-  // console.log('data on calendar Post', data);
+  // throw new Error('Este e um erro do calendario que o Bruno criou!')
+  const data = await req.json();
+  console.log('data on calendar Post', data);
   
-  // return NextResponse.json(scheduleData);
+  return NextResponse.json(scheduleData);
 }
